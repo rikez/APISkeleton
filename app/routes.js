76cdbc147145067c9f91630cf96abfea4d7dbbb7
Enrico Alvarenga <enricomalvarenga@gmail.com>
2017-06-27 00:27:35 -0300

    Changing middlewares object

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,14 @@
 module.exports = (app) => {
     const routes = {
         client: app.controllers.client.clientController(),
-        clientForm: app.middlewares.forms.client.clientForm,
         auth: app.controllers.auth.authController(),
+    }
+
+    const middlewares = {
+        clientForm: app.middlewares.forms.client.clientForm,
         authForm: app.middlewares.forms.auth.authForm,
     }
 
-    app.post("/v1/api/client.py", routes.clientForm, routes.client.saveClient)
-    app.post("/v1/api/auth.py", routes.authForm, routes.auth.getAuthToken)
+    app.post("/v1/api/client.py", middlewares.clientForm, routes.client.saveClient)
+    app.post("/v1/api/auth.py", middlewares.authForm, routes.auth.getAuthToken)
 }
